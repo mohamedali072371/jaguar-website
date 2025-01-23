d0fefee1a7c1dfd67d299890de3bd8e2ceeb1e56
Mohamed Ali <mohamed@astasystech.com>
2025-01-23 16:52:23 +0530

    tyre branding glb updated

--- public/ar/js/main.js
+++ public/ar/js/main.js
@@ -1,11 +1,27 @@
+import {  getMobileOS, mobileOSConstant } from './constant/constant.js';
+
 const modelViewerTyre = document.querySelector("model-viewer#animated");
-const animationStopTime = 11;
-modelViewerTyre.addEventListener("load", async () => {
-        
+const animationStopTime = 18;
+const width = window.innerWidth; 
+
+deafultCameraPosition();   
+
+modelViewerTyre.addEventListener("load", async () => { 
     const arButton = document.getElementById('ar-button');
     arButton.style.display = "block";
     playAnimation();
 
+    arButton.addEventListener("click", () => {
+        const osType = getMobileOS();
+        if (osType == mobileOSConstant.ios) {
+          const link = document.createElement('a');
+          link.href = "./assets/glb/tyre-branding.reality";
+          link.id = "ar-link";
+          link.rel = "ar";
+      
+          link.click();
+        }
+    });
 
     document.addEventListener('click', async (event) => {
         const material = modelViewerTyre.materialFromPoint(event.clientX, event.clientY);
@@ -19,7 +35,6 @@ modelViewerTyre.addEventListener("load", async () => {
     });
 
 })
-
 
 modelViewerTyre.addEventListener('ar-status', async (event) => { 
     if (event.detail.status == 'session-started') {
@@ -49,4 +64,17 @@ const stopAnimationAtTime = () => {
     } else {
         requestAnimationFrame(stopAnimationAtTime); // Keep checking the time
     }
+}
+
+
+
+function deafultCameraPosition() {
+    console.log(width)
+    if (width <= 480) { 
+        console.log('Works')
+        modelViewerTyre.setAttribute("camera-orbit", '2.177deg 86.32deg 15.11m');
+        modelViewerTyre.setAttribute("camera-target", '-1m 1m 0.001144m');
+        // modelViewerTyre.setAttribute("field-of-view", "18deg");
+        // Mobile devices (<= 480px)
+    } 
 }
